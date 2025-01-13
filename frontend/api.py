95e66e212bd67084b7724188a9abe576651aba3a
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2025-01-13 01:59:04 +0000

    uptade

--- frontend/api.py
+++ frontend/api.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import pickle
+
 
 app = Flask(__name__)
 
@@ -12,7 +14,19 @@ def api_music():
 	req = request.get_json(force=True)
 	print(req['songs'])
 
-	return jsonify({'songs':['oi', 'tudo?'], 'version': 1})
+
+	app.model = pickle.load(open("./ml_rules/model_rules.pkl", "rb"))
+
+	x = os.path.getmtime("./ml_rules/model_rules.pkl")
+    if x != app.last_modified == pickle.load:
+        app.model = pickle.load(open("./ml_rules/model_rules.pkl", "rb"))
+        app.last_modified = os.path.getmtime("./ml_rules/model_rules.pkl")
+
+	playlist_ = ['Song 1', 'Song 2', 'Song 3']
+
+
+	
+	return jsonify({'songs': playlist_, 'version': 1, 'model_date': "date"})
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0', port=52061)
--- ml_rules/ml_code_music.py
+++ ml_rules/ml_code_music.py
@@ -3,12 +3,14 @@ import ssl
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 import pickle
-
+import os
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-df = pd.read_csv("https://homepages.dcc.ufmg.br/~cunha/hosted/cloudcomp-2023s2-datasets/2023_spotify_ds1.csv")
+url = os.environ.get('url')
+
+df = pd.read_csv(url)
 
 basket = df.groupby(['pid', 'track_name'])['track_name'].count().unstack().fillna(0)
 
@@ -24,5 +26,5 @@ basket_sets = basket.applymap(encode_units)
 frequent_itemsets = apriori(basket_sets, min_support=0.07, use_colnames=True)
 rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
 
-rules.to_pickle("~/project2-pv2/model_rules.pkl")
+rules.to_pickle("./ml_rules/model_rules.pkl")
 
